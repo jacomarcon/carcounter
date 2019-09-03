c022a5dfa17dcd9a9aebaacd2c7b5f502b5435f3
Jacob Hunt <jacob@myrnix.com>
2019-09-03 10:04:17 -0400

    initial commit

---
+++ src/carcounter.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "c:/projects/particle/carcounter/src/carcounter.ino"
+/*
+ * Project carcounter
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "c:/projects/particle/carcounter/src/carcounter.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
